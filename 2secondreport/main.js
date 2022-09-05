4e6f19589ef81dda5deb592dce489b250175b3b6
Ahmed Zena <ahmedzena53@gmail.com>
2022-09-05 16:43:17 +0200

    the second challenge

---
+++ 2secondreport/main.js
@@ -0,0 +1,14 @@
+let a = 10 , b = "20" , c =80 ;
+//console.log (++a + +b++ + +c++ - +a++);  // 100
+
+// لو نفذت الكود دا علطول بدون عمل الكود الاول ك كومنت
+// that will override to the last values oa a,b,c 
+// console.log (++a + -b + +c++ - -a++ + +a) ; //94  
+
+
+
+
+// لو نفذت الكود دا علطول بدون عمل الكود الاول ك كومنت
+// that will override to the last values oa a,b,c 
+
+console.log ( --c + +b + --a * +b++ - +b *a + --a - +true  ) ;   //97
